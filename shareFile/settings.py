088e182764d72180b720063767d0453b6af72a85
Hilary <70286503+Hilary139@users.noreply.github.com>
2023-02-22 22:14:39 +0100

    Add files via upload

--- shareFile/settings.py
+++ shareFile/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://xcloud.up.railway.app/'
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
